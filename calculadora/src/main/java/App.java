b0d80483848efed18d2c272e5aa6f3b26f32333b
rafa19888cruz <rafa08cruz19888@gmail.com>
2022-05-12 22:26:09 -0300

    modificando a apresentaçao dos resultados

--- calculadora/src/main/java/App.java
+++ calculadora/src/main/java/App.java
@@ -42,19 +42,7 @@ public class App {
             default:
                 System.out.println("A equação não existe");
         }
-        /*
-         * Recebendo rec = new Recebendo();
-         * 
-         * Soma soma = new Soma(2,8);
-         * Dividir divi = new Dividir(10, 5);
-         * Multiplicar mult = new Multiplicar(2,6);
-         * Subtrair sub = new Subtrair(15, 5);
-         * 
-         * System.out.println("Soma : " + rec.CalculaOperacao(soma));
-         * System.out.println("Subtração : " + rec.CalculaOperacao(sub));
-         * System.out.println("Divisão : " + rec.CalculaOperacao(divi));
-         * System.out.println("Multiplicação : " + rec.CalculaOperacao(mult));
-         */
+        
     }
 
 }
