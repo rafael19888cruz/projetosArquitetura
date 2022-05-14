151a8db596fce47e64b269ee29fcca67cf590b7c
rafa19888cruz <rafa08cruz19888@gmail.com>
2022-05-13 21:21:33 -0300

    inserindo Scanner no App.java para capturar entrada de dados

--- calculadora/src/main/java/App.java
+++ calculadora/src/main/java/App.java
@@ -12,29 +12,42 @@ public class App {
         switch (i) {
 
             case 1:
-                System.out.println("a soma de 2 com 8");
-                Soma soma = new Soma(2, 8);
-                 System.out.println("Soma : " + rec.CalculaOperacao(soma));
+            System.out.println("Informe os valores");
+            
+            int num1 = v.nextInt();
+            int num2 = v.nextInt();
+            Soma soma = new Soma(num1,num2);
+               
+             System.out.println("Soma : " + rec.CalculaOperacao(soma));
                 break;
 
 
             case 2:
-            System.out.println("a subtração de 15 com 5");
-                Subtrair sub = new Subtrair(15, 5);
-                System.out.println("Subtração : " + rec.CalculaOperacao(sub));
+            System.out.println("Informe os valores");
+            
+            int num3 = v.nextInt();
+            int num4 = v.nextInt();
+            Subtrair sub = new Subtrair(num3,num4);
+            System.out.println("Subtração : " + rec.CalculaOperacao(sub));
                 break;
 
 
             case 3:
-            System.out.println("a multiplicação de 2 com 6");
-                Multiplicar mult = new Multiplicar(2, 6);
+            System.out.println("Informe os valores");
+            
+            int num5 = v.nextInt();
+            int num6 = v.nextInt();
+                Multiplicar mult = new Multiplicar(num5, num6);
                 System.out.println("Multiplicação : " + rec.CalculaOperacao(mult));
                 break;
 
 
             case 4:
-            System.out.println("a divisão de 10 com 5");
-                Dividir divi = new Dividir(10, 5);
+            System.out.println("Informe os valores");
+            
+            int num7 = v.nextInt();
+            int num8 = v.nextInt();
+                Dividir divi = new Dividir(num7, num8);
                 System.out.println("Divisão : " + rec.CalculaOperacao(divi));
                 break;
 
